0e8ef9298385f3c00b511b35efd850a97859c745
Fabian Rump <rump@adesso-mobile.de>
2025-09-28 16:41:24 +0200

    removed mvvm solution

--- composeApp/src/commonMain/kotlin/de/fabianrump/crossnotes/ui/feature/home/mvvm/HomeScreenState.kt
+++
@@ -1,9 +0,0 @@
-package de.fabianrump.crossnotes.ui.feature.home.mvvm
-
-import de.fabianrump.crossnotes.domain.models.Todo
-
-internal data class HomeScreenState(
-    val dailyUsefulInfo: String? = null,
-    val pastTodosExists: Boolean = false,
-    val todos: List<Todo> = emptyList()
-)
